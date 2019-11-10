8a4b22ee51c54b118e70109b1ab5622e8ff83ab8
vitor-giacomelli <kruxer1985@hotmail.com>
2019-11-10 12:59:06 -0200

    Create input entry and lenght cron validation

--- CronParser/Program.cs
+++ CronParser/Program.cs
@@ -2,11 +2,32 @@
 
 namespace CronParser
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter Cron string");
+            var cronString = Console.ReadLine();
+            ValidateCronString(cronString);
+        }
+
+        private static void ValidateCronString(string cronString)
+        {
+            try
+            {
+                var valueList = cronString.Split(' ');
+                if (valueList.Length != 5)
+                {
+                    throw new InvalidCastException();
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("Invalid Cron lenght. Please enter five values separated by a single space.");
+                Console.ReadKey();
+            }
             
+
         }
     }
 }
