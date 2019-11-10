3b7634f274c42b366d7b3a47a2605aaa29c737ee
vitor-giacomelli <kruxer1985@hotmail.com>
2019-11-10 15:21:48 -0200

    Implement Cron Validator test for Minute field with valid values

--- CronParserTests/CronTests.cs
+++ CronParserTests/CronTests.cs
@@ -1,16 +1,19 @@
 using CronParser.Entities;
 using NUnit.Framework;
 using Shouldly;
+using System.Collections.Generic;
+using static CronParser.Program;
 
 namespace Tests
 {
     public class CronTests
     {
         private Cron _cronTestObject;
+        private CronValidator _cronValidator;
         [SetUp]
         public void Setup()
         {
-            
+
         }
 
         [Test]
@@ -26,6 +29,37 @@ namespace Tests
                 () => _cronTestObject.Month.ShouldBe("0"),
                 () => _cronTestObject.DayOfMonth.ShouldBe("0"),
                 () => _cronTestObject.DayOfWeek.ShouldBe("0"));
+        }
+
+        private static IEnumerable<TestCaseData> CronValidator_ValidMinuteDataSource()
+        {
+            return new[]
+            {
+                new TestCaseData("0", "0", "0", "0", "0"),
+                new TestCaseData("*", "0", "0", "0", "0"),
+                new TestCaseData("?", "0", "0", "0", "0")
+            };
+        }
+
+        [TestCaseSource(nameof(CronValidator_ValidMinuteDataSource))]
+        public void CronValidator_ValidMinute_IsValidTrue(
+            string minute, 
+            string hour, 
+            string dayOfMonth, 
+            string month, 
+            string dayOfWeek)
+        {
+            // Arrange
+            string[] cronValues = new string[5];
+            cronValues[0] = minute;
+            cronValues[1] = hour;
+            cronValues[2] = dayOfMonth;
+            cronValues[3] = month;
+            cronValues[4] = dayOfWeek;
+            _cronValidator = new CronValidator(cronValues);
+
+            // Act, Assert
+            _cronValidator.IsValid.ShouldBeTrue();
         }
     }
 }
